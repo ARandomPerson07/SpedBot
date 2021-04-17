a5ec88a0a5d3bcc6167d8a8487decdeb6a56a1a3
wgbachan <>
2021-04-17 17:54:59 +0000

    - Added bot command _reg - added db support to save chara lists

--- main.py
+++ main.py
@@ -3,12 +3,62 @@ import os
 import random
 from replit import db
 from uptime import keep_alive
+from discord.ext import commands
 
 client = discord.Client()
 speed_state = False
 
 speed_d = {}
 game = discord.Game("something, probably")
+bot = commands.Bot(command_prefix = "_")
+res_dict = {"I":"Ineffective","E":"Endured",
+            "N":"Normal","W":"Weak", "F":"Fatal"}
+
+if 'char_dict' not in db.keys():
+  char_dict = {}
+  db['char_dict']=char_dict
+
+class character:
+  def __init__(self, name = 'you forgot the name, congrats!',
+              strength = 0, dex = 0, wit = 0, cha = 0, 
+              s_res = 'Fatal', p_res = 'Fatal', b_res = 'Fatal', passive = "None"):
+        self.name = name
+        self.str = strength
+        self.dex = dex
+        self.wit = wit
+        self.cha = cha
+        self.s_res = s_res
+        self.p_res = p_res
+        self.b_res = b_res
+
+  def stats(self):
+    'Returns a discord embed with the character\'s stats'
+    embed = discord.Embed(Title = f"{self.name}'s Stats",
+                          colour = 0x13E8AE )
+    embed.add_field(name = "Stats",
+                    value = f"STR: {self.str}\nDEX: {self.dex} \nWIT: {self.wit}\
+                    \nCHA: {self.cha}")
+    embed.add_field(name =  "Resists",
+                    value = f"Slash: {self.s_res}\nPierce: {self.p_res}\n\
+                    Blunt:{self.b_res}")
+    return embed
+
+@bot.command(name = 'reg', help = 'Syntax: _reg [name] [S/W/D/C] [S/P/B]\nAccepted Resists are I/E/N/W/F\n\n e.g. _reg Adam 0/0/0/0 F/F/F')
+async def register(ctx, name, stats, resists):
+  stat_list = stats.split('/')
+  if len(stat_list)!=4:
+    await ctx.send("Please provide 4 stats in S/D/W/C format")
+    return
+  res_list = resists.split('/')
+  if len(res_list)!=3:
+    await ctx.send("Please provide the three resistances in S/P/B format")
+    return
+  globals()[name] = character(name,stat_list[0],stat_list[1],stat_list[2],stat_list[3],
+            res_list[0],res_list[1],res_list[2])
+  char_dict[name] = globals()[name]
+  db['char_dict'] = char_dict
+  
+
 
 @client.event
 async def on_connect():
@@ -26,8 +76,9 @@ async def on_message(message):
     global speed_state
     global speed_d
 
-    if message.author != client.user:
+    if message.author != client.user and '_' in message.content:
 
+        #_speed command
         if '_speed' in message.content:
             print('detected _speed command')
 
@@ -54,6 +105,8 @@ async def on_message(message):
                     await message.channel.send(speed_message + "**")
                 speed_d = {}
 
+
+        #_set command
         if speed_state:
             print('speedstate detected true')
             if '_set' in message.content:
@@ -73,7 +126,7 @@ async def on_message(message):
                       print("Please enter the same number of speeds and characters!")
                     for i,character in enumerate(t_inputs):
                       speed_d[t_inputs[i]] = int(s_inputs[i])+random.randint(0,100)/1000
-
+    await bot.process_commands(message)
 
 @client.event
 async def on_disconnect():
